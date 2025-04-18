d1e2e2a51103e18d6487c03d4c41536a1eff9111
Dwayne <dune.njenga@student.moringaschool.com>
2025-04-18 21:11:32 +0300

    Completed

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,21 @@
 // Your tests here
+import {isPalindrome} from "../utils"
+
+describe("isPalindrome", () => {
+    it("returns true when the word is the same forwards and backwards", () =>{
+        const word = "racecar";
+
+        const result = isPalindrome(word);
+        
+
+        expect(result).toBe(true);
+
+    });
+    it("return true for words that are a combination of uppercase and lowercase letters", () => {
+        const word = "RaCecar";
+    
+        const result = isPalindrome(word);
+    
+        expect(result).toBe(true);
+      });
+})
